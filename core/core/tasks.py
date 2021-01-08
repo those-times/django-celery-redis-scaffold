6ab44a0feef1a5eed0a1219a0ba944dcad5c6d3a
Volodymyr Yanov <vyanov@vyanov-macbook-pro.local>
2021-01-08 13:10:12 -0800

    moved app folder to /usr/src show celery logging

--- core/core/tasks.py
+++ core/core/tasks.py
@@ -1,7 +1,11 @@
 from celery import shared_task
 import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 @shared_task
 def add(x, y):
+    logger.info("HELLO THERE!")
     time.sleep(10)
     return x + y
